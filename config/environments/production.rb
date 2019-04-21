1f3be3018218c790c91ab292b2eeb9a726d8c30e
Serge Lafontaine <slafontaine10@gmail.com>
2019-04-21 13:18:20 -0400

    moved to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "juniorgolf_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => 'juniorgolf.heroku.com' }
+
 
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
