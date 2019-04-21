7150654f22d59a24e4075b9921e501346d2aba46
Serge Lafontaine <slafontaine10@gmail.com>
2019-04-21 13:16:09 -0400

    updated environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+config.action_mailer.default_url_options = { :host => 'juniorgolf.heroku.com' }
