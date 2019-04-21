bdf159820d79c88e0cc80b4a5da7549cc885bac4
Serge Lafontaine <slafontaine10@gmail.com>
2019-04-21 13:30:04 -0400

    updated gmail userid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['juniorgolftracker.com'],
+   :user_name            => ENV['juniorgolftracker'],
    :password             => ENV['Number10'],
    :authentication       => "plain",
   :enable_starttls_auto => true
